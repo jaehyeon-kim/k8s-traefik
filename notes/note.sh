867be647333d4ca0da71b78a80955eefed077986
Jaehyeon Kim <dottami@gmail.com>
2019-11-23 07:35:50 +0000

    up notes

---
+++ notes/note.sh
@@ -0,0 +1,4 @@
+https://rogerwelin.github.io/traefik/reverse/proxy/micro/services/2018/09/17/traefik-tutorial.html
+https://rogerwelin.github.io/traefik/api/go/auth/2019/08/19/build-external-api-with-trafik-go.html
+https://sysadmins.co.za/integrating-google-oauth-with-traefik/
+https://docs.traefik.io/v1.7/configuration/backends/docker/
